f82441bd8def47822e9ac7f8663953ab191862b3
crispymangoes <crispymangoes@gmail.com>
2022-09-06 14:23:44 -0700

    Add in other prettier files

--- src/RewardDistributor.sol
+++ src/RewardDistributor.sol
@@ -4,16 +4,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-
-struct RewardInfo{
-    address token;
-    uint amount;
-}
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 
 /**
  * @title Adds advanced reward distribution logic to a staking contract
@@ -21,69 +17,62 @@ struct RewardInfo{
  * @notice on every reward deposit the summation of rewardDeposit/totalBalance is saved and used to calculate
  * a users reward share
  * @notice Airdrops are handled using an ERC20 snapshot
- * @dev _updateReward must be called before a users userBalance changes, and  before _claimRewards is called 
+ * @dev _updateReward must be called before a users userBalance changes, and  before _claimRewards is called
  * @dev Need to implement a deposit function that calls _mint
  * @dev need to implement a withdraw function that calls _burn
  */
-abstract contract RewardDistributor is Ownable, ERC20{
+abstract contract RewardDistributor is Ownable, ERC20, Pausable {
     using SafeERC20 for ERC20;
 
     //reward tracking
-    mapping(address => uint) public rewardCount;//tracks amount of times rewards are added
-    mapping(address => mapping(uint => uint)) public cumulativeRewardShare;//store cumulative reward share as rewards are added
-    mapping(address => bool) public isRewarder;
+    uint256 public rewardCount; //tracks amount of times rewards are added
+    mapping(uint256 => uint256) public cumulativeRewardShare; //store cumulative reward share as rewards are added
 
     //user information
-    mapping(address => mapping(address => uint)) public rewardCountLastClaim;//store users last claimed reward
-    mapping(address => mapping(address => uint)) public rewardOwed;//store reward owed to user
+    mapping(address => uint256) public rewardCountLastClaim; //store users last claimed reward
+    mapping(address => uint256) public rewardOwed; //store reward owed to user
     mapping(address => address) public payoutTo;
 
-    bool public paused;
-
     ERC20 private immutable rewardToken;
-
-    modifier checkPause{
-        require(!paused, "RewardDistributor: Contract is paused");
-        _;
-    }
 
     /**
      * @param _name the name of the staked token users get for joining pool
      * @param _symbol the symbol of the staked token users get for joining the pool
      */
-    constructor(string memory _name, string memory _symbol, ERC20 _rewardToken) ERC20(_name, _symbol) Ownable(){
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        ERC20 _rewardToken
+    ) ERC20(_name, _symbol) Ownable() {
         rewardToken = _rewardToken;
     }
 
     /****************************external onlyOwner *************************************/
-    function adjustRewarder(address _rewarder, bool _state) external onlyOwner{
-        isRewarder[_rewarder] = _state;
+    function Pause() external onlyOwner {
+        _pause();
     }
 
-    function Pause() external onlyOwner{
-        paused = true;
-    }
-
-    function unPause() external onlyOwner{
-        paused = false;
+    function unPause() external onlyOwner {
+        _unpause();
     }
 
     /****************************external mutative *************************************/
 
-    function setPayoutTo(address _to) external{
+    function setPayoutTo(address _to) external {
         payoutTo[msg.sender] = _to;
     }
 
-    function depositReward(uint _amount) external checkPause{
-        require(isRewarder[msg.sender], "RewardDistributor: Caller is not a rewarder");
+    function depositReward(uint256 _amount) external whenNotPaused {
         rewardToken.safeTransferFrom(msg.sender, address(this), _amount);
-        
-        uint count = rewardCount[address(rewardToken)];
-        cumulativeRewardShare[address(rewardToken)][count] = cumulativeRewardShare[address(rewardToken)][count-1] + (_amount/totalAmountDeposited());
-        rewardCount[address(rewardToken)]+=1;
+
+        uint256 count = rewardCount;
+        cumulativeRewardShare[count] =
+            cumulativeRewardShare[count - 1] +
+            (_amount / totalAmountDeposited());
+        rewardCount += 1;
     }
 
-    function claimRewards(address _user) external virtual checkPause{
+    function claimRewards(address _user) external virtual whenNotPaused {
         _claimRewards(_user);
     }
 
@@ -92,55 +81,64 @@ abstract contract RewardDistributor is Ownable, ERC20{
     /**
      * @dev should return a users balance
      */
-    function userBalance(address _user) public view virtual returns(uint){
+    function userBalance(address _user) public view virtual returns (uint256) {
         return balanceOf(_user);
     }
 
     /**
      * @dev should return the total amount of deposit in the contract
      */
-    function totalAmountDeposited() public view virtual returns(uint){
+    function totalAmountDeposited() public view virtual returns (uint256) {
         return totalSupply();
     }
 
-    function pendingRewards(address _user) public view returns(uint256 reward){
-        uint clc;//count last claim
-        uint cc;//current count
-        clc = rewardCountLastClaim[address(rewardToken)][_user];
-        cc = rewardCount[address(rewardToken)] - 1;
-        if(cc == clc){
+    function pendingRewards(address _user)
+        public
+        view
+        returns (uint256 reward)
+    {
+        uint256 clc; //count last claim
+        uint256 cc; //current count
+        clc = rewardCountLastClaim[_user];
+        cc = rewardCount - 1;
+        if (cc == clc) {
             return 0; //user already claimed rewards for this token
         }
-        reward = rewardOwed[address(rewardToken)][_user] + userBalance(_user) * (cumulativeRewardShare[address(rewardToken)][cc] - cumulativeRewardShare[address(rewardToken)][clc]); 
+        reward =
+            rewardOwed[_user] +
+            userBalance(_user) *
+            (cumulativeRewardShare[cc] - cumulativeRewardShare[clc]);
     }
 
     /****************************internal mutative *************************************/
     /**
      * @dev must be called before a users deposit changes, and before a user claims rewards
      */
-    function _updateRewards(address _user) internal{
-        uint clc;//count last claim
-        uint cc;//current count
-        clc = rewardCountLastClaim[address(rewardToken)][_user];
-        cc = rewardCount[address(rewardToken)] - 1;
-        if(cc == clc){
+    function _updateRewards(address _user) internal {
+        uint256 clc; //count last claim
+        uint256 cc; //current count
+        clc = rewardCountLastClaim[_user];
+        cc = rewardCount - 1;
+        if (cc == clc) {
             return; //user already claimed rewards for this token
         }
-        rewardOwed[address(rewardToken)][_user] += userBalance(_user) * (cumulativeRewardShare[address(rewardToken)][cc] - cumulativeRewardShare[address(rewardToken)][clc]);
-        rewardCountLastClaim[address(rewardToken)][_user] = cc;
+        rewardOwed[_user] +=
+            userBalance(_user) *
+            (cumulativeRewardShare[cc] - cumulativeRewardShare[clc]);
+        rewardCountLastClaim[_user] = cc;
     }
 
     //could allow a user to only claim on certain tokens?  If they pass in a token array
-    function _claimRewards(address _user) internal{
+    function _claimRewards(address _user) internal {
         require(_user != address(0), "RewardDistributor: Invalid Address");
         _updateRewards(_user);
         address to = _user;
-        if(payoutTo[_user] != address(0)){
+        if (payoutTo[_user] != address(0)) {
             to = payoutTo[_user];
         }
-        uint owed;
-        owed = rewardOwed[address(rewardToken)][_user];
-        rewardOwed[address(rewardToken)][_user] = 0;
+        uint256 owed;
+        owed = rewardOwed[_user];
+        rewardOwed[_user] = 0;
         rewardToken.safeTransfer(to, owed);
     }
 
@@ -151,6 +149,9 @@ abstract contract RewardDistributor is Ownable, ERC20{
         uint256 amount
     ) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
-        require(from == address(0) || to == address(0), "Reward Distributor: Token transfers are not allowed");
+        require(
+            from == address(0) || to == address(0),
+            "Reward Distributor: Token transfers are not allowed"
+        );
     }
 }
